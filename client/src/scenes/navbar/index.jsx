a0d44fb8f95475a95c46a5ebca0d59cc3c025f94
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-11-22 22:02:07 -0500

    added adverts

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          JoeSocial
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -24,12 +24,11 @@ const AdvertWidget = () => {
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>AntiVirus</Typography>
+        <Typography color={medium}>AntiVirus.com</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+        Do not forget to wear protection from viruses.
       </Typography>
     </WidgetWrapper>
   );
